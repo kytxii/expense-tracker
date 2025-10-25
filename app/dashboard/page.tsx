747df6f30432203f0aa11c83a2203500132357cf
kytxii <tilleykyle12@gmail.com>
2025-10-24 20:58:14 -0700

    Small theme/color changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,26 +7,26 @@ const DashboardPage = () => {
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
-      <aside className="bg-slate-100 w-64 shadow-[5px_0px_10px_rgba(0,0,0,0.3)] border-r-2 border-slate-300">
+      <aside className="bg-white w-64 border-r-2 border-slate-300">
         <Sidebar />
       </aside>
 
       {/* Main Section */}
       <div className="flex flex-col flex-1">
         {/* Header */}
-        <header className="bg-slate-100 h-16 flex items-center border-b-2 border-slate-300">
+        <header className="bg-white h-16 flex items-center border-b-2 border-slate-300">
           <Header />
         </header>
 
         {/* Content */}
-        <main className="flex-1 bg-slate-100 overflow-y-auto p-4">
+        <main className="flex-1 bg-white overflow-y-auto p-4">
           {/* Main content goes here */}
         </main>
 
         {/* Footer */}
-        <footer className="bg-slate-100 h-12 flex items-center justify-center border-t-2 border-slate-300">
+        {/* <footer className="bg-white h-12 flex items-center justify-center border-t-2 border-slate-300">
           <Footer />
-        </footer>
+        </footer> */}
       </div>
     </div>
   );
