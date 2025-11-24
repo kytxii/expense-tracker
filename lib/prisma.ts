9718730bc5c162f90e1d15cf2a393690bab56ace
kytxii <tilleykyle12@gmail.com>
2025-11-24 00:19:12 -0700

    Added Prisma schema, client configuration, and models

---
+++ lib/prisma.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const clientOptions: any = {
+  log: ["query"],
+};
+
+if (process.env.DATABASE_URL) {
+  clientOptions.adapter = {
+    url: process.env.DATABASE_URL,
+  };
+}
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient(clientOptions);
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
