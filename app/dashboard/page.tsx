56845848ee1f5303848fd305e8f8a47d71ee12b0
kytxii <tilleykyle12@gmail.com>
2025-11-30 15:35:21 -0700

    Implemented the Dashboard user authentication and data fetching

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { prisma } from "../../lib/prisma";
+import { redirect } from "next/navigation";
 
 import Header from "../components/page_ui/Header";
 import Sidebar from "../components/page_ui/Sidebar";
 import Footer from "../components/page_ui/Footer";
 import TotalAmount from "../components/features/visualizations/TotalAmount";
 import BarChart from "../components/features/visualizations/BarChart";
+import DataTable from "../components/page_ui/DataTable";
 
 const summaryTitles = [
   { label: "Total Income" },
@@ -13,7 +17,28 @@ const summaryTitles = [
   { label: "Total Bills" },
 ];
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const { userId } = await auth();
+
+  {
+    /* If user is not logged in, redirect to login page */
+  }
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+
+  {
+    /* Get all entries for the user */
+  }
+  const entries = await prisma.entry.findMany({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      date: "desc",
+    },
+  });
+
   return (
     <div className="flex min-h-screen">
       {/* Sidebar */}
@@ -29,7 +54,7 @@ const DashboardPage = () => {
         </header>
 
         {/* Content */}
-        <main className="flex flex-col p-10 justify-center mt-19">
+        <main className="flex flex-col p-10 justify-center mt-4">
           <div className="flex flex-row justify-between">
             {summaryTitles.map((item) => (
               <TotalAmount key={item.label} title={item.label} />
@@ -37,6 +62,21 @@ const DashboardPage = () => {
           </div>
           <div className="flex justify-center mt-10">
             <BarChart />
+          </div>
+          {/* Header row */}
+          <div className="flex w-full mb-2 gap-2 mt-10">
+            <span className="w-1/2 bg-[var(--bg-component)] rounded-md text-left p-3 text-xl font-bold">
+              Name
+            </span>
+            <span className="w-1/4 bg-[var(--bg-component)] rounded-md text-left p-3 text-xl font-bold">
+              Date
+            </span>
+            <span className="w-1/4 bg-[var(--bg-component)] rounded-md text-left p-3 text-xl font-bold">
+              Amount
+            </span>
+          </div>
+          <div className="bg-[var(--bg-component)] rounded-md p-2 ">
+            <DataTable entries={entries} />
           </div>
         </main>
 
